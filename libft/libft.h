bedb41f2e6cb7dea5122df3cbe61ad98f2b6e22c
araiva <kizzaraiva@gmail.com>
2022-09-24 23:18:56 +0700

    add strcmp in libft

--- libft/libft.h
+++ libft/libft.h
@@ -33,6 +33,7 @@ int		ft_tolower(int c);
 int		ft_toupper(int c);
 int		ft_strlen(const char *str);
 int		ft_strncmp(const char *str1, const char *str2, size_t n);
+int		ft_strcmp(const char *s1, const char *s2);
 size_t	ft_strlcpy(char *dest, const char *src, size_t size);
 size_t	ft_strlcat(char *dest, const char *src, size_t size);
 char	*ft_strnstr(const char *str, const char *find, size_t size);
