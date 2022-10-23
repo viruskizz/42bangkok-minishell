ee2cf610d21d1cbdf80d7dcbd253808e09aeb41d
araiva <kizzaraiva@gmail.com>
2022-10-24 04:14:02 +0700

    add testfile

--- test.sh
+++ test.sh
@@ -1,128 +1,92 @@
 #!/bin/bash
-
+#
 # Setup
 RESTART_CMD="make restart"
-TEST_FILE=tfile
-EXPECT_FILE=efile
+# TEST_FILE=testcmd
+TEST_FILE=
+VERB=0
+EXPECT_FILE=expecttest
 PREFIX_COMMAND="input command"
-
-# Testcase
-## You can add your test here
-TESTS=(
-  "echo a\""
-  "echo sr*"
-  "echo \"sr*\""
-  "echo abc"
-  "echo \"xyz\""
-  "echo \"xyz\"'123'"
-  "echo a\"x\"y\"z\"a        ijk"
-  "echo ~"
-  "echo '123'"
-  "echo xyz'123'xyz"
-  "echo 'x'\$HOME'x'"
-  "echo 'x'~'x'"
-  "echo"
-  "echo '\"$HOME\"'"
-  "echo \"'$HOME'\""
-  "echo $?"
-  "echo abc$HOME"
-  "echo abc$HOMEde"
-  "echo \"abc$HOMEde\""    
-  "echo 'abc$HOMEde'"
-  "echo abc$?de"
-  "echo \"abc$?de\""
-  "echo 'abc$?de'"
-  "echo \"10\"\"20\"\"30\""
-  "echo \"10\"20\"30\""
-  "echo '10''20''30'"
-  "echo '10'20'30'"
-  "echo 10\"20\"'30'"
-  "echo 10\"20     \"'30     '"
-  "echo 10 \"20     \"'30     '"
-  "echo 10    \"20     \"'30     '"
-  "echo 10    \"20     \" '30     '"
-  "echo 10    \"20     \"     '30     '"
-  "echo \"\$HOME\" '\$HOME'"
-)
-
-EXPECTS=(
-  "Error unexpected token"
-  "srcs"
-  "sr*"
-  "abc"
-  "xyz"
-  "xyz123"
-  "axyza ijk"
-  "$HOME"
-  "123"
-  "xyz123xyz"
-  "x/home/araivax"
-  "x~x"
-  ""
-  "$HOME"
-  "'\"\$HOME\"'"
-  "0" #(exit status)
-  "abc\/Users\/username"
-  "abc"
-  "abc"
-  "abc${HOME}de"
-  "abc0de"
-  "abc0de"
-  "abc$?de"
-  "102030"
-  "102030"
-  "102030"
-  "102030"
-  "102030"
-  "1020     30"
-  "10 20     30"
-  "10 20     30"
-  "10 20      30"
-  "10 20      30" 
-  "$HOME \$HOME"
-)
 
 ### DO NOT change code below ###
 ##
 # Constants
+GRAY="\033[0;37m"
 CYAN="\033[0;36m"
+GREEN="\033[0;32m"
+YELLOW="\033[0;33m"
 RESET="\033[0m"
-IFILE=input.txt
-OFILE=output.txt
+IFILE=test/input.txt
+RFILE=test/result.txt
+OFILE=test/output.txt
+EFILE=test/expect.txt
+DFILE=test/diff.txt
 
-# Generate Test
-if [ -f $TEST_FILE ]; then
-  cp $TEST_FILE $IFILE
-else
-  echo -n "" > $IFILE
-  for t in "${TESTS[@]}"
-  do
-      echo -e "$t" >> $IFILE
-  done
-fi
+# Get option from command line
 
-# Runner
-echo -e "$CYAN""minishell test script"$RESET"\nby Araiva"
-echo "========================"
-$RESTART_CMD
-./minishell < $IFILE > $OFILE && cat $OFILE
-cat $OFILE | grep -v "$PREFIX_COMMAND" > tmp && cp tmp $OFILE && /bin/rm tmp;
-
-n=$(cat $OFILE | wc -l)
-i=1
-while [ $i -le $n ]
-do
-  sed -n "$i,1p" $OFILE
-  $((i++))
+while getopts "hvf:" options; do
+  case "$options" in
+    f)
+      TEST_FILE=$OPTARG
+      ;;
+    v)
+      VERB=1
+      ;;
+    h)
+      echo "Usage: $0 [ -f test file ] [ -v verbose ] [-h help]" 
+      ;;
+    *)
+      echo "Usage: $0 [ -f test file ] [ -v verbose ] [-h help]" 
+      exit 1
+      ;;
+  esac
 done
 
-#Cleasing
-if [ -f $IFILE ]; then
-  /bin/rm $IFILE
-fi
+main() {
+  echo $TEST_FILE
+  cat $TEST_FILE
+}
 
-# if [ -f $OFILE ]; then
-#   /bin/rm $OFILE
-# fi
+generate() {
+    if [ ! -d "test/" ]
+    then
+      mkdir test/
+    fi
+    if [ ! -f $TEST_FILE ]
+    then
+      echo "please add your test command in $TEST_FILE"
+      echo "echo example" > $TEST_FILE
+      exit 1
+    else
+      cp $TEST_FILE $IFILE
+    fi
+}
 
-exit 0
+runner() {
+  # Runner
+  echo -e "$CYAN""minishell test script"$RESET"\nby Araiva"
+  echo "========================"
+  $RESTART_CMD
+  ./minishell < $IFILE > $RFILE
+  while read -r line; do eval "$line"; done < $IFILE > $EFILE
+  cat $RFILE
+  cat $RFILE | grep -v "$PREFIX_COMMAND" > $OFILE
+
+  n=$(cat $OFILE | wc -l)
+  i=1
+  while [ $i -le $n ]
+  do
+    # echo -e $GRAY   "$i:" $(sed -n "$i,1p" $IFILE) $RESET
+    # echo -e $YELLOW ">:" $(sed -n "$i,1p" $OFILE) $RESET
+    # echo -e $CYAN   "+:" $(sed -n "$i,1p" $EFILE) $RESET
+    # echo -e $CYAN   "+:" `eval "$(sed -n "$i,1p" $IFILE)"` $RESET
+    # EXPECT=`eval $(sed -n "$i,1p" $IFILE)`
+    # echo -e $EXPECT >> $EFILE
+    # echo -e $CYAN   "+:" $EXPECT $RESET
+    i=$(( i + 1 ))
+  done
+
+  diff $OFILE $EFILE > $DFILE
+}
+
+main; exit 0
